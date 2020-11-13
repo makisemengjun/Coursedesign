8c26b9d4649ceaf07942381017b3cbecd7212819
liziyi_laptop <makisemengjun@gmail.com>
2020-11-13 10:08:04 +0800

    Java课上结束，故同步。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        /*
+        /**
         bili tmp = new bili();
         String strmp;
         long trid;
         strmp = bili.get_real_url(26445, 10000, "web");
         System.out.println(strmp);
-*/
+        */
+        try{
+            MairaDB.select("select * from ");
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
