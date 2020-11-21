158bb4131e5dc2ba545d8ff273e6a42ae7d0c439
liziyi <makisemengjun@gmail.com>
2020-11-21 15:30:01 +0800

    重整代码

--- src/com/company/ExecThread.java
+++ src/com/company/ExecThread.java
@@ -5,21 +5,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class ExecThread extends Thread{
+public class ExecThread extends Thread {
     InputStream is;
     String type;
-    ExecThread(InputStream is,String type){
-        this.is=is;
-        this.type=type;
+
+    ExecThread(InputStream is, String type) {
+        this.is = is;
+        this.type = type;
     }
-    public void run(){
+
+    public void run() {
         try {
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String line = null;
             while ((line = br.readLine()) != null)
                 System.out.println(line);
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
