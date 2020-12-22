480d07011c9c2aa81be234452f8f00fd1cc1215e
liziyi <makisemengjun@gmail.com>
2020-12-22 19:53:38 +0800

    douyu.java已经完成，需要重整下代码。

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        //gui.print();
-        try {
-            douyu a = new douyu(9190899, 0);
-            System.out.println(a.get_real_url());
-        }
-        catch (CException e){
-            ;
-        }
+        gui.print();
     }
 
 }
--- src/com/company/douyu.java
+++ src/com/company/douyu.java
@@ -11,6 +11,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -39,7 +40,8 @@ public class douyu {
         t10 = String.valueOf(ltmp / 1000);
         //以下用正则表达式找到房间号
         String tmp_url = "https://m.douyu.com/" + short_id;
-        String result_get_rid = MyHttp.get(tmp_url).toString();
+        StringBuilder sb_tmp = MyHttp.get(tmp_url);
+        String result_get_rid = sb_tmp.toString();
         Pattern p_rid = Pattern.compile("\"rid\":(\\d{1,7})");
         Matcher matcher = p_rid.matcher(result_get_rid);
         if (matcher.find()) {
@@ -54,17 +56,16 @@ public class douyu {
 
     private String md5(String data) {
         String ans = "";
-        try{
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-        md5.update(data.getBytes());//TODO :: may require utf-8 settings
-        BigInteger hash = new BigInteger(1, md5.digest());//符号
-        ans = hash.toString(16);
-        while (ans.length() < 32) {
-            ans = "0" + ans;
-        }
-        return ans;
-        }
-        catch (NoSuchAlgorithmException e){
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(data.getBytes());//TODO :: may require utf-8 settings
+            BigInteger hash = new BigInteger(1, md5.digest());//符号
+            ans = hash.toString(16);
+            while (ans.length() < 32) {
+                ans = "0" + ans;
+            }
+            return ans;
+        } catch (NoSuchAlgorithmException e) {
 
         }
         return ans;
@@ -78,34 +79,73 @@ public class douyu {
         String cdn = "ws-h5";
         String res = MyHttp.get("https://www.douyu.com/" + rid).toString();
         Pattern p_js_func = Pattern.compile
-                ("(vdwdae325w_64we[\\s\\S]*function ub98484234[\\s\\S]*?)function");
+                ("(var\\svdwdae325w_64we[\\s\\S]*function ub98484234[\\s\\S]*?)function");
         Matcher m_js_func = p_js_func.matcher(res);
         String result = "", func_ub9;
         if (m_js_func.find()) {
             result = m_js_func.group(1);
         }
-        func_ub9 = result.replaceAll("eval.*?;", "strc;}");
+        func_ub9 = result.replaceAll("eval.*?;}", "strc;}");
         ScriptEngineManager m = new ScriptEngineManager();
-        ScriptEngine engine = m.getEngineByName("JavaScript");
-        String v,rb,func_sign;
+        ScriptEngine engine = m.getEngineByName("js");
+        String v, rb, func_sign, params;
         try {
-            engine.eval(func_ub9);
+            try {
+                engine.eval(func_ub9);
+            } catch (Exception e) {
+
+            }
             Invocable inv = (Invocable) engine;
             res = inv.invokeFunction("ub98484234").toString();
-            System.out.println(res);
             p_js_func = Pattern.compile("v=(\\d+)");
             m_js_func = p_js_func.matcher(res);
-            if(m_js_func.find()){
-                v= m_js_func.group(1);
-                rb = this.md5(rid+did+t10+v);
+            if (m_js_func.find()) {
+                v = m_js_func.group(1);
+                rb = this.md5(rid + did + t10 + v);
+                func_sign = res.replaceAll("return rt;}\\);?",
+                        "return rt;}");
+                func_sign = func_sign.replace("(function (",
+                        "function sign(");
+                func_sign = func_sign.replace("CryptoJS.MD5(cb).toString()"
+                        , "\"" + rb + "\"");
+                engine.eval(func_sign);
+                inv = (Invocable) engine;
+                params = inv.invokeFunction("sign", rid, did, t10).toString();
+                String tmp = "&cdn=RECDN&rate=RERATE";
+                tmp = tmp.replace("RECDN", cdn);
+                tmp = tmp.replace("RERATE", "0");
+                params += tmp;
+                String url = "https://www.douyu.com/lapi/live/getH5Play/" + rid + "?" + params;
+                CloseableHttpClient client = HttpClients.createDefault();
+                HttpPost post = new HttpPost(url);
+                CloseableHttpResponse response = client.execute(post);
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        response.getEntity().getContent()));
+                StringBuilder builder = new StringBuilder();
+                while ((tmp = br.readLine()) != null) {
+                    builder.append(tmp);
+                }
+                br.close();
+                response.close();
+                client.close();
+                JSONObject jsonObject = new JSONObject(builder.toString());
+                jsonObject = jsonObject.getJSONObject("data");
+                tmp = jsonObject.getString("rtmp_live");
+                System.out.println(tmp);
+                Matcher matcher = Pattern.compile("(\\d{1,7}[0-9a-zA-Z]+)").matcher(tmp);
+                if (matcher.find()) {
+                    tmp = matcher.group(1);
+                }
+                return tmp;
             }
 
 
         } catch (ScriptException e) {
-
-        }
-        catch (NoSuchMethodException e){
-            System.out.println("code error");
+            System.out.println("script exception");
+        } catch (NoSuchMethodException e) {
+            System.out.println("code error no such method");
+        } catch (IOException e) {
+            System.out.println("code error IO");
         }
 
         return "";
@@ -141,10 +181,10 @@ public class douyu {
             err_id = json_res.getLong("error");
             Boolean json_data_null = false;
             JSONObject json_data = null;
-            try{
+            try {
                 json_data = json_res.getJSONObject("data");
-            }catch (JSONException e){
-                json_data_null=true;
+            } catch (JSONException e) {
+                json_data_null = true;
             }
 
             key_id = "";
@@ -164,16 +204,19 @@ public class douyu {
 
     public String get_real_url() throws CException {
         this.get_pre();
-        if (err_id!=0){
-            key_id = this.get_pc_js();
+        if (err_id == 0) {
+            ;
         }
         if (err_id == 102) {
             throw new CException("房间不存在");
         } else if (err_id == 104) {
             throw new CException("主播未开播");
+        } else {
+            //throw new CException("暂不支持此类型");
+            key_id = this.get_pc_js();
         }
-        return "http://tx2play1.douyucdn.cn/live/REPLACE.flv?uuid="
-                .replace("REPLACE", key_id);
+
+        return "http://tx2play1.douyucdn.cn/live/" + key_id + ".flv?uuid=";
 
     }
 }
